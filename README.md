9bcb495fae730d94ef9b4812554951f1ed8407df
yvan <yvanscher@gmail.com>
2022-11-05 15:34:55 +0100

    update

--- README.md
+++ README.md
@@ -1,88 +1,16 @@
+# intro
+
+the basic issue with pdf scraping is that the data is structured visually in a way that the underlying data is not. there are different solutions to solve this. here what i use [textract](textract.readthedocs.io/) python library which uses ocr to generate a stream of text data.
 
 
+# method
 
+use textract to generate the text data stream
+then use regex to look for certain patterns to divide thise stream into blocks
+connect blocks together that may share data (name + email), blocks located close to each other in the stream
+search the blocks for connected data and produce a final structure
 
-# issue with essy pdfs
-
-problem 1 emails don't match names.
-problem 2 some names are split on 2 lines
-and so we only get one part (like Byron) of the name
-
-what i could do is to match names and emails somehow like in a soup.
-i could rank the emails by likelihood of beloning to the person.
-
-
-the problem is block data is split up by principal name and email,
-they are coming in separate blocks.
-
-GRADES PRINCIPAL AND SCHOOL ALL FOLLOW EACH OTHER
-BUT EMAILS NO. THEY ARE IN A DIFFERENT COLUMN
-THE DATA IS READ AS COLUMNS BY PAGE SO WE COULD JUST TRY MATCHING INDICES
-BY PAGE
-OR WE COULD TAKE some window around the principal data and look for email by string matching.
-OR BOTH INDICES TO REDUCE SEARCH AREA, STRING MATCHING TO CONFIRM.
-this is probably a typical pattern in pdfs.
-
-but how to know when we've passed to a new column?? the extraction algo would know.
-but the data is not given i nthe byte stream or elsewhere. see a library with that
-kind of structured info would be very useful page limits, sides and bottoms.
-most have a FAX number at the top which we could regex...
-if we match the regex... for a phone number... we can then assume the start of the 3rd column? might owrk.
-
-
-OK institution id and principal data (the first 2 columns) actually come together. then phone numbers come after
-so what we can do is... build an number of indices -+2 for the last emails and then when the regex match hits
-
-
-start at _ SD _OR institution id
-build index pausing counting when we hit 2 linefeeds 
-hit phone number regex
-check indices in window,
-string match names to emails,
-end at last email
-restart at next institution id.
-
-so the scanner reads the SD then it reads the left part of the the first block, then the right, then left then right all the way down. then it reads the phone numbers as a separate column/block all the way down.
-
-the difference between "blocks" or within blocks seems to be 3 blank lines. ok it's actually 2 linefeeds in a row.
-
-i cannot seem to get teh regex to detect 2 linefeeds in a row to work... you should be able to just copy some pattern prtined and then it generates some regex or code to capture it. the problem is not interesting b ut it is a pain and a common painpoint.
-
-no but... it IS a line as all i'm doing is printing the test lines...
-
-ok i'm a bit tired of all of this. the structure in the pdf is too unreliable. it does not break nicely into columns
-
-i'm going to grab every name and split it on whitespace, put it in a dict.
-grab every email and split it on @ and .
-
-then toss all those things into a dict. (the emails have to connect to the phone numbers so we still want the blocks).
-
-then once we match a name and email via the dict, pull out also the phone numbers from maybe a separate dict.
-
-the pattern is
-
-Name:
-
-Roland Hobson
-
-Email:
-
-roland.hobson@BLA
-
-rhobson@BLA
-hobsonr@BLA
-
-rolandh@BLA
-hroland@BLA
-
-ok
-
-
-
-ok a neasier way would be...
-we have soem current block
-it builds when it hits the SD match
-it appends into the overall block when we hit a linefeed.
+other resources:
 
 https://betterprogramming.pub/how-to-convert-pdfs-into-searchable-key-words-with-python-85aab86c544f
 https://github.com/py-pdf/PyPDF2
--- oregon_deptedu_11102022/main.py
+++ oregon_deptedu_11102022/main.py
@@ -1,34 +1,10 @@
-"""
--first understand how the stream of data comes out from textract or py2pdf or tabula-py
--second produce an index of this data stream, preferably in one or two passes, the index
-is like an object, database or set of relations that connects the data intelligently,
-for example by section, visually, and then drawing connections between "adjacent" sections,
-or something that can reliably map names and emails together.
--third use the index to construct a final dataset with names, emails, numbers, whatever
-the client is looking for
-- four quality control, check a sample of the output for quality, flaws, etc.
-- five get early client feedback on data quality, iterate and improve.
-- one thing is that when someone has a short name like "thao do," since
-  do appears in a bunch of other words it has a huge hit rate.
-- another thing is hits from someone's name like "will" to the name of a district
-  like willamina.
-- another thing to realize is that school, not district
-  determines the email address.
-- you can match the names to emails (same string), and emails to phones (same data block)
-  but you will struggle to match the school to that info, becuase it comes at semi
-  random times in the stream.
-- some even have a single letter which makes you match L against every email in the 5 block spread, for example
-
-- string comparisons in pdb seem to take forever??
-
-"""
-
 import os
 import re
 import json
 import textract
 from itertools import product
 from collections import OrderedDict
+
 
 def numNameMatch(name, email):
     names = name.split()
@@ -41,26 +17,22 @@ def getPdfText(inpath,outpath):
 
 def getData(textfile):
     with open(textfile, 'rb') as f:
+        # get the lines from the pdf
         lines = [line.decode('utf-8') for line in f]
+
+        # compile a regex for detecting each school district
         regex_school_district = re.compile("[a-zA-Z0-9 ]*\sSD[\sa-zA-Z0-9]*")
-        #regex_phone = re.compile("[0-9]{3}-[0-9]{3}-[0-9]{4}")
-
-        # first flag
-        fst_flag = False
-
-        testflag = False
-
-        aggblock = []
-        principals = OrderedDict()
-        previousblock = ""
-        currentblock = ""
-        blockname = ""
-        blockmail = ""
 
         # this section collects data into a dictionary, called principals
         # basically it organized the data by the names of the principals
         # so we can limit our search later to look within a particular block
         # i.e. from one _SD_ match to another
+        principals = OrderedDict()                
+        fst_flag = False
+        currentblock = ""
+        blockname = ""
+        blockmail = ""
+        aggblock = []
         
         for i,line in enumerate(lines):         
             match_sd = re.search(regex_school_district, line)
@@ -92,7 +64,8 @@ def getData(textfile):
 
                 aggblock = []
                 
-        # create previous mappings
+        # add previous and next blocks to the
+        # data because it's a bit scrambled
         windowidx = 1
         previous = []
         keys = list(principals.keys())
@@ -103,13 +76,14 @@ def getData(textfile):
                 previous.pop(0)
             previous.append(key)
             
-
+        # in this section build a dictionary
+        # of names and emails for every school
+        # district
         namesEmails = {k: {} for k in principals.keys()}
         
         # for each school district
         for sd,nm in principals.items():
-            # add values from prior
-            # and next
+            # add values from prior/next
             for nextprev in nm['previous'] + nm['next']:
                 for name, blockvalues in principals[nextprev]['names'].items():
                     if nm['names'].get(name) is None:
@@ -120,7 +94,7 @@ def getData(textfile):
             
             names = list(nm["names"].keys())
             mails = list(nm["mails"].keys())
-            
+
             for name in names:
                 # split on whitespace
                 ns = name.split()
@@ -146,26 +120,17 @@ def getData(textfile):
                                              "emails":matches,
                                              "phones":[nm["mails"][mail][:2] for mail in matches],}
                     
-        
-        # print(principals)
-        # print(namesEmails)
+
+        # print info about data
         print(json.dumps(namesEmails,indent=2))
         print("entries")
         print(len(namesEmails))
         print("entries with more than 0 thing")
         print(len([1 for v in namesEmails.values() if len(v) > 0]))
+
+        # save to a file
         with open("output.json", 'w') as f:
             json.dump(namesEmails,f,indent=2)
-
-        """
-        the SD is not a good separator because sometimes (for reasons i don't understand)
-        it reads the next one before the email section of the SD you'r currently on.
-
-        that means we need a bit of a different strategy, instead of seaching inside one "SD"
-        we can maybe search a window?? dunno.
-
-        could just try the simple thing to include... previous block and next block??
-        """
                 
             
 if __name__ == "__main__":
